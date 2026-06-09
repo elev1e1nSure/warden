5317dc2e2aab44babd79706921229237d4d1268e
elev1e1n <elev1e1n@gmail.com>
2026-06-09 18:02:11 +0300

    add: mouse support for text selection

--- go/main.go
+++ go/main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	p := tea.NewProgram(initialModel(), tea.WithAltScreen())
+	p := tea.NewProgram(initialModel(), tea.WithAltScreen(), tea.WithMouse())
 	if _, err := p.Run(); err != nil {
 		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
