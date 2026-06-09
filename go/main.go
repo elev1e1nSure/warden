e7fa22465e910f3ffed79533e2b63f36f3a5b80a
elev1e1n <elev1e1n@gmail.com>
2026-06-09 17:45:38 +0300

    fix: remove invalid WithFocus option

--- go/main.go
+++ go/main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	p := tea.NewProgram(initialModel(), tea.WithAltScreen(), tea.WithFocus(nil))
+	p := tea.NewProgram(initialModel(), tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
 		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
